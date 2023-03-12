242b99cb9026463e1d257e3710bffb443e61a3ed
joeldonaire <joelator29@gmail.com>
2023-03-12 20:14:03 +0100

    arreglo de eliminar

--- react/src/places/PlaceMarks.jsx
+++ react/src/places/PlaceMarks.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { delMark } from '../slices/placeMarkSlice.'
+import { useDispatch } from 'react-redux';
 
 // const PlaceMarks = ({ mark,handleDeleteMark}) => {
 
 const PlaceMarks = ({ mark }) => {
+  const dispatch=useDispatch();
   console.log(mark)
   return (
 
@@ -12,7 +14,7 @@ const PlaceMarks = ({ mark }) => {
       <td>{mark.description}</td>
       <td><a href={mark.ruta}>{mark.ruta}</a></td>
 
-      <td><button onClick={()=>delMark(mark.id)  }>🗑️</button></td>
+      <td><button onClick={()=>{dispatch(delMark(mark.id))}  }>🗑️</button></td>
       {/* <td><button onClick={()=>handleDeleteMark(mark.id)  }>🗑️</button></td> */}
 
     </tr>
